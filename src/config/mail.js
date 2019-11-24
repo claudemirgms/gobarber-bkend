b03fa155c0a01b2ec47c111fb6a6af26ad4f7faf
Claudemir Gomes dos Santos <claudemirgms@gmail.com>
2019-11-24 16:59:30 -0300

    config nodemailer (send mail cancel appointment

---
+++ src/config/mail.js
@@ -0,0 +1,20 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  segure: false,
+  auth: {
+    user: '650e10ef5fcd63',
+    pass: '7e31f3fa1ba3c2',
+  },
+  default: {
+    from: 'go barber',
+  },
+};
+/*
+Amazon SES
+Mailgun
+Sparkpost
+Mandril (mailchimp)
+
+Mailtrap (DEV)
+*/
