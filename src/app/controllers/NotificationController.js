c66a8f4895b7b58011ec8d66b040b8f0a0c6329a
Claudemir Gomes dos Santos <claudemirgms@gmail.com>
2019-11-24 15:48:30 -0300

    mark appointment as read

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -20,6 +20,18 @@ class NotificationController {
       .limit(20);
     return res.json(notifications);
   }
+
+  async update(req, res) {
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      {
+        read: true,
+      },
+      { new: true }
+    );
+
+    return res.json();
+  }
 }
 
 export default new NotificationController();
